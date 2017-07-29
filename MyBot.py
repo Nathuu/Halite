1f2bb8a6d7f05ff8b9377431b5eb1e3277a9c782
Nathuu <marc.dupuis@usherbrooke.ca>
2017-07-29 10:49:00 -0400

    Get to the nearest border

--- MyBot.py
+++ MyBot.py
@@ -6,24 +6,13 @@ myID, game_map = hlt.get_init()
 hlt.send_init("PythonBot")
 
 
-def assign_move(square):
-    for direction, neighbor in enumerate(game_map.neighbors(square)):
-        if neighbor.owner != myID and neighbor.strength < square.strength:
-            return Move(square, direction)
-
-    if square.strength < 5 * square.production:
-        return Move(square, STILL)
-    else:
-        return Move(square, random.choice((NORTH, WEST)))
-
-
 def findNearestEnemyDirection(loc):
     CARDINALS = NORTH, EAST, SOUTH, WEST, STILL
     direction = NORTH;
-    
+
     maxDistance = min(game_map.width, game_map.height) / 2;
 
-    for direction, in CARDINALS :
+    for direction, in CARDINALS:
         distance = 0
         current = loc
         site = game_map.getSite(current, direction)
@@ -32,14 +21,23 @@ def findNearestEnemyDirection(loc):
             current = game_map.getLocation(current, direction)
             site = game_map.getSite(current)
 
-
         if (distance < maxDistance):
             newDirection = direction
             maxDistance = distance
 
-
-
     return newDirection
+
+def assign_move(square):
+    for direction, neighbor in enumerate(game_map.neighbors(square)):
+        if neighbor.owner != myID and neighbor.strength < square.strength:
+            return Move(square, direction)
+
+    if square.strength < 5 * square.production:
+        return Move(square, STILL)
+    else:
+        return Move(square, findNearestEnemyDirection(square))
+
+
 
 
 while True:
