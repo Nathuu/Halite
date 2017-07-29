c6f31c6b2a7f8a5a867d722cc47b4ee2d98f07c6
Nathuu <marc.dupuis@usherbrooke.ca>
2017-07-29 10:14:17 -0400

    init commit

--- MyBot.py
+++ MyBot.py
@@ -2,13 +2,18 @@ import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
 
-
 myID, game_map = hlt.get_init()
-hlt.send_init("MyPythonBot")
+hlt.send_init("PythonBot")
+
+
+def assign_move(square):
+    if square.strength == 0:
+        return Move(square, STILL)
+    else:
+        return Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL)))
+
 
 while True:
     game_map.get_frame()
-    # target = game_map.get_direction_toward_with_A_star(square, game_map.contents[0][0])
-    # donne le direction pour aller vers la case 0 a partir du carre donne
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
+    moves = [assign_move(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
